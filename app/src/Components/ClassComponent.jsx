7bc3293448ec182520c76113301272c6ac900f6c
Ushlerdop <axalimon@gmail.com>
2021-12-29 17:45:44 +1000

    Timer in both components

--- app/src/Components/ClassComponent.jsx
+++ app/src/Components/ClassComponent.jsx
@@ -1,14 +1,35 @@
 import React, { Component } from 'react';
+import '../App.css';
 
 class ClassComponent extends Component {
-  isClassToggle = () => {
-    this.props.isClassToggle();
+  constructor(props) {
+    super(props);
+    this.state = { seconds: this.props.time };
+    this.setClassComponentTime = this.props.setClassComponentTime.bind(this);
+    this.isClassToggle = this.props.isClassToggle.bind(this);
+    this.timer = this.timer.bind(this);
+  }
+
+  componentDidMount() {
+    this.timerId = setInterval(this.timer, 100);
+  }
+
+  componentWillUnmount() {
+    this.setClassComponentTime(this.state.seconds);
+    clearInterval(this.timerId);
+  }
+
+  timer() {
+    this.setState({
+      seconds: this.state.seconds + 0.1,
+    })
   }
 
   render() {
     return (
       <div>
         <h1>Классовая компонента</h1>
+        <p className='timer'>Вы смотрите на этот компонент приблизительно <span>{this.state.seconds.toFixed(1)} сек.</span></p>
         <button onClick={this.isClassToggle}>Поменять на функциональную компоненту</button>
       </div>
     );
--- app/src/Components/FuncComponent.jsx
+++ app/src/Components/FuncComponent.jsx
@@ -1,9 +1,18 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import useInterval from '../Utils/useInterval';
+import '../App.css';
 
-const FuncComponent = ({isClassToggle}) => {
+const FuncComponent = ({isClassToggle, setFuncComponentTime, time}) => {
+  let [seconds, setSeconds] = useState(time);
+  useEffect(() => {
+    return setFuncComponentTime(seconds);
+  })
+  useInterval(() => setSeconds(seconds+0.1), 100);
+
   return (
     <div>
       <h1>Функциональная компонента</h1>
+      <p className='timer'>Вы смотрите на этот компонент приблизительно <span>{seconds.toFixed(1)} сек.</span></p>
       <button onClick={isClassToggle}>Поменять на классовую компоненту</button>
     </div>
   );
